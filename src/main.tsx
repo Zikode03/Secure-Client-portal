bcb2428226b755532236d85d5e48074f0556a216
Zikode03 <ngcebonkwanyana03@gmail.com>
2026-09-14 14:33:02 +0200

    apply focused document register styling

--- src/main.tsx
+++ src/main.tsx
@@ -13,6 +13,7 @@ import { PortalProvider } from "./app/portal";
 import { ThemeProvider } from "./app/theme";
 import "./styles.css";
 import "./kpi-compact.css";
+import "./documents-register.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
