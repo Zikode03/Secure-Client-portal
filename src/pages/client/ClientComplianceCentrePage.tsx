317c2942f7d1d4131ab94c65d9b1d6affae19f5e
Zikode03 <ngcebonkwanyana03@gmail.com>
2026-09-16 16:21:48 +0200

    Show compliance verification on client centre

--- src/pages/client/ClientComplianceCentrePage.tsx
+++ src/pages/client/ClientComplianceCentrePage.tsx
@@ -1,5 +1,9 @@
 import { ClientComplianceCentreWorkspace } from "../../components/compliance/ClientComplianceCentreWorkspace";
+import { ClientComplianceVerificationPanel } from "../../components/compliance/ClientComplianceVerificationPanel";
 
 export function ClientComplianceCentrePage() {
-  return <ClientComplianceCentreWorkspace />;
+  return <>
+    <ClientComplianceCentreWorkspace />
+    <ClientComplianceVerificationPanel />
+  </>;
 }
